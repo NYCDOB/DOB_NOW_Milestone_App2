393478d02ce3d89322d169a8c7828a5f2ab55c91
bmancell@buildings.nyc.gov <BMancell@buildings.nyc.gov>
2019-04-23 08:59:48 -0400

    First commit

---
+++ js/jscript.js
@@ -0,0 +1,54 @@
+var selectedCharacteristic;
+
+  // update the about text
+function updateTextArea(characteristic) {
+  var thisCharacteristic = characteristics[characteristic];
+  $('#char-display-name').text(thisCharacteristic.displayName);
+  $('#char-about').text(thisCharacteristic.about);
+}
+
+function updateChartMap(characteristic) {
+		var thisMapCharacteristic = characteristics[characteristic];
+		$('#chart-map').prop('src', thisMapCharacteristic.chartMap);
+		//$('#chart-map').src = thisMapCharacteristic.chartMap;
+}
+
+Object.keys(characteristics).forEach(function (d) {
+  d3.select('.char-select')
+    .append('button')
+    .attr('id', d)
+    //.attr('class', 'btn btn-default btn-xs')
+	.attr('class', 'btn btn-primary btn-md')
+    .text(characteristics[d].displayName);
+});
+
+//  kick things off by downloading the csv
+d3.csv('data/historic_migration_selchars.csv', function (data) {
+
+  // change the selected characteristic when the user clicks a button
+  $('.char-select>button').click(function () {
+    selectedCharacteristic = $(this)[0].id;
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+
+
+    updateTextArea(selectedCharacteristic);
+	updateChartMap(selectedCharacteristic);
+
+    // set url hash
+    history.replaceState(undefined, undefined, '#' + selectedCharacteristic);
+  });
+
+  // select hashed characteristic
+  if (Object.keys(characteristics).indexOf(window.location.hash.split('#')[1]) > -1) {
+    $(window.location.hash).click();
+  } else {
+    $('#accident').click();
+  }
+});
+
+
+// trigger download
+// $('#csv-download').click(function () {
+
+// });
---
+++ js/topics.js
@@ -0,0 +1,51 @@
+var characteristics = { // eslint-disable-line
+  workType: {
+    displayName: 'Work Type',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    colors: ['#1B83B0', '#C17229'],
+    about: 'Approved CN permits include the use of various equipment types, including derricks, fix/climber tower cranes, mast climbers, mobile cranes, mobile tower cranes, and pile drivers. There was a steady increase in the number of approved CN permits since 2012, with an 82% increase from 2012 to 2016 and a 23% increase from 2015 to 2016. In Manhattan, CD105 had the greatest number of CN permits approved in 2012 and 2013, while CD104 experienced the greatest number of permits approved in 2014 and 2015. In Brooklyn, CD302 experienced a spike of approved CN permits in 2014, with a 279% increase from the previous year.',
+    descriptor: 'people who reported as'
+  },
+  geography: {
+    displayName: 'Geography',
+	chartMap: 'https://nycdob.github.io/D3_Cranes/CraneAccidents.html',
+    colors: ['#a6d1f8', '#012e60'],
+    about: 'The number of crane accidents in 2016 has increased by 80% from the previous year and 157% from 2007. Manhattan experienced the majority of accidents within the 10 year period of 2007 - 2016, accounting for 70% of all crane accidents, while Staten Island and the Bronx had the least number of accidents (1% and 3% respectively). The crane types most associated with an accident are tower cranes accounting for 48% of the accidents occurring between 2007 and 2016. Mobile truck cranes were the second highest crane type associated with an accident accounting for 33% of accidents. There were a total of 37 injuries and 5 fatalities associated with crane accidents between 2007 and 2016. The greatest number of injuries associated with a crane accident occurred in 2013 and 2015 (9 injuries each year). 2016 experienced the greatest number of fatalities associated with a crane accident with a total of 3 (2 of which occurred at the same site on the same date in Queens). Manhattan experienced the greatest number of injuries and fatalities between 2007 and 2016 accounting for 62% of the total. Queens experienced the second greatest number of injuries and fatalities accounting for 24% of the total. The crane type associated with the greatest number of injuries and fatalities are mobile truck cranes accounting for 50% of the total.',
+    descriptor: 'people aged'
+  },
+  timeLine: {
+    displayName: 'Time Line',
+	chartMap: 'https://nycdob.github.io/ActiveB_AvgAge_Map/',
+    colors: ['#a6d1f8', '#012e60'],
+    about: 'Of all characteristics, age is one of the greatest predictors of migration. NYC consistently attracts large numbers of people in their 20s, and generally sees net migration losses of people in all other age groups. This is tied to the lifecycle of migration, whereby young single people move to the city, and leave after family formation. Over the past 30 years, net migration among those in their 20s has grown more positive.  While net migration was still negative for most other age groups, the most recent period has seen a reduction in net losses for most age groups.',
+    descriptor: 'people aged'
+  }
+  // cd: {
+    // displayName: 'Certificate of Operation',
+	// chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    // colors: ['#1B83B0', '#C17229'],
+    // about: 'Historically, males and females are generally equally likely to move in or out of the city. However, in the 2010-2014 period, NYC experienced a net gain of 44,000 more women than men due to migration.',
+    // descriptor: 'people who reported as'
+  // },
+  // type: {
+    // displayName: 'Equipment Type',
+	// chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    // colors: ['#1f4064', '#fdc010', '#cd594a', '#73c4e1', '#5e170e'],
+    // about: 'Each race/Hispanic group has shown a different pattern over the last 30 years. The City’s fiscal crisis in the 1970s saw a big loss of the white population, with smaller losses in the decades that followed. 2010-2014 shows positive net migration for white New Yorkers. The black population has shown consistent net migration losses since the 70s, a reversal of the trend in the earlier part of the 20th century that saw net migration for the black population surge, especially from the southern states.  Hispanics, too, have experienced net migration losses since the 70s, but their losses have been paired back and were close to zero in the latest time period.  Asians are the only major race/ethnic group to have positive net migration since 1975.',
+    // descriptor: 'people who reported their race as'
+  // },
+	// age: {
+	// displayName: 'Equipment Age',
+	// chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+	// colors: ['#a6d1f8', '#012e60'],
+	// about: 'Of all characteristics, age is one of the greatest predictors of migration. NYC consistently attracts large numbers of people in their 20s, and generally sees net migration losses of people in all other age groups. This is tied to the lifecycle of migration, whereby young single people move to the city, and leave after family formation. Over the past 30 years, net migration among those in their 20s has grown more positive.  While net migration was still negative for most other age groups, the most recent period has seen a reduction in net losses for most age groups.',
+	// descriptor: 'people aged'
+  // },
+  // ahv: {
+    // displayName: 'After Hours Variances',
+	// chartMap: 'https://nycdob.github.io/D3_Cranes/CranePermitsApproved_2012_2016_Map.html',
+    // colors: ['#1B83B0', '#C17229'],
+    // about: 'Historically, males and females are generally equally likely to move in or out of the city. However, in the 2010-2014 period, NYC experienced a net gain of 44,000 more women than men due to migration.',
+    // descriptor: 'people who reported as'
+  // }
+};
